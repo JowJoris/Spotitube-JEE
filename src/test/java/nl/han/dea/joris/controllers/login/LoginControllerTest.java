b7552dd3f968c0bfddd10200a47f3698c3d1ee24
JowJoris <32440632+JowJoris@users.noreply.github.com>
2018-10-04 12:02:36 +0200

    Hopelijk werkt het nu wel

---
+++ src/test/java/nl/han/dea/joris/controllers/login/LoginControllerTest.java
@@ -0,0 +1,12 @@
+package nl.han.dea.joris.controllers.login;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+    @Test
+    public void doSomething(){
+        Assert.assertEquals(1,1);
+    }
+}
