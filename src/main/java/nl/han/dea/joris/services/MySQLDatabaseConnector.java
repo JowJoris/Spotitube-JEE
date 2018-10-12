82ce6326003f16eb905ef5eca85b679a9a8aaeed
JowJoris <32440632+JowJoris@users.noreply.github.com>
2018-10-12 10:22:34 +0200

    Fixed smell: password in connection

--- src/main/java/nl/han/dea/joris/services/MySQLDatabaseConnector.java
+++ src/main/java/nl/han/dea/joris/services/MySQLDatabaseConnector.java
@@ -5,9 +5,8 @@ import java.sql.*;
 public class MySQLDatabaseConnector {
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/Spotitube?user=root&password=";
         try {
-            DriverManager.getConnection(url);
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/Spotitube?user=root&password=");
         } catch (SQLException e) {
             e.printStackTrace();
         }
