e4169e626a536d9c344f999d7282f503203b409e
JowJoris <32440632+JowJoris@users.noreply.github.com>
2018-10-18 17:39:45 +0200

    Fix code smells

--- src/main/java/nl/han/dea/joris/services/UserService.java
+++ src/main/java/nl/han/dea/joris/services/UserService.java
@@ -9,28 +9,47 @@ public class UserService {
     private String user;
     private String token;
 
-    public String getUser() {return user;}
-    public String getToken() {return token;}
+    public String getUser() {
+        return user;
+    }
 
-    public boolean authenticate(String username, String password){
-        Connection connection;
-        connection = connector.getConnection();
+    public String getToken() {
+        return token;
+    }
+
+    public boolean authenticate(String username, String password) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
         try {
-            PreparedStatement pstmt = connection.prepareStatement("SELECT * FROM `userdata` WHERE user = ? AND password = ?");
+            connection = connector.getConnection();
+
+            pstmt = connection.prepareStatement("SELECT * FROM `userdata` WHERE user = ? AND password = ?");
             pstmt.setString(1, username);
             pstmt.setString(2, password);
-            ResultSet rs = pstmt.executeQuery();
-            if (!rs.first()){
+
+            rs = pstmt.executeQuery();
+
+            if (!rs.first()) {
                 return false;
             }
             if (rs.first()) {
                 this.user = rs.getString("user");
                 this.token = rs.getString("token");
             }
-            pstmt.close();
-            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null && pstmt !=null && rs != null) {
+                try {
+                    connection.close();
+                    pstmt.close();
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return true;
     }
