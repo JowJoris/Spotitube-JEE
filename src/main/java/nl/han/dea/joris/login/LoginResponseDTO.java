b874f4324b767120a741798859f9317497713615
JowJoris <32440632+JowJoris@users.noreply.github.com>
2018-10-20 19:11:39 +0200

    100% test score LoginResponseDTO

--- src/main/java/nl/han/dea/joris/login/LoginResponseDTO.java
+++ src/main/java/nl/han/dea/joris/login/LoginResponseDTO.java
@@ -5,19 +5,6 @@ public class LoginResponseDTO {
     private String user;
     private String token;
 
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
+    public void setUser(String user) {this.user = user;}
+    public void setToken(String token) {this.token = token;}
 }
